2f02a169bef6643dd4b97d1de147085df6a67c4b
Joaobolito22 <joao.bolito@baraodemaua.br>
2025-03-27 16:29:37 -0300

    trazendo os dados da API com base no username do usuario

--- proj_api_github/resources/views/usuariosApiGithub.blade.php
+++ proj_api_github/resources/views/usuariosApiGithub.blade.php
@@ -4,11 +4,75 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script defer>
+        function getApi(){
+            const ul = document.querySelector('ul')
+            ul.innerHTML = ''
+            const user = document.querySelector('#user').value
+
+            fetch(`https://api.github.com/users/${user}/repos`)
+                .then(async resposta => {
+
+                    let data = await resposta.json()
+
+                    data.map(item => {
+                        let li = document.createElement('li')
+                        li.classList.add('m-10')
+
+                        li.innerHTML = `<strong>${item.name}</strong>`  // Aqui, você insere o nome do repositório.
+
+                        ul.appendChild(li)
+
+                        // Se você quiser adicionar uma linha horizontal entre os itens, pode fazer assim:
+                        let hr = document.createElement('hr')
+                        ul.appendChild(hr)
+                    })
+                })
+                .catch(e => {
+                    console.log(e);
+                })
+        }
+    </script>
     <title>Lista de Usuários</title>
+
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+        }
+
+        body{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        hr{
+            width: 50vw;
+            margin: 10px;
+        }
+
+        .m-10{
+            margin: 10px;
+        }
+
+        li{
+            display: flex;
+            flex-direction: column;
+        }
+    </style>
 </head>
 <body>
-    <main>
-        <h1>Lista de usuários API GitHub</h1>
-    </main>
+    <h1 class="m-10">Github API</h1>
+    <div>
+        <label for="">Nome do Usuário</label>
+        <input type="text" id="user">
+        <button onclick="getApi()">Buscar</button>
+    </div>
+    <hr>
+    <ul class="m-10">
+
+    </ul>
+
 </body>
 </html>
